e3a106166e5a6df17eeb6680fbaeab3708386423
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2025-07-14 20:22:12 +0600

    completed the 404 page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const error = useRouteError()
+    console.log(error);
+
     return (
-        <div>
-            <h2 className='text-5xl'>Page Not Found</h2>
-            <p>Status: 404</p>
+        <div className='text-center'>
+            <p className='text-5xl my-5'>{error.statusText || error.message}</p>
+            {
+                error.status === 404 && <div>
+                    <p>Go back to where you are from</p>
+                    <Link to={'/'}><button className='btn btn-lg btn-outline text-purple-600 font-semibold rounded-4xl'>Home</button></Link>
+                </div>
+            }
         </div>
     );
 };
